90f728d2b5a29d83b357a492ee2d2188686619bb
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-04-18 02:23:15 +0700

    Update_Tampilan_Chart_Finish

--- app/Views/global/viewchart.php
+++ app/Views/global/viewchart.php
@@ -1,6 +1,7 @@
 <?= $this->extend('layout/template') ?>
 
 <?= $this->section('content') ?>
+<link rel="stylesheet" href="<?= base_url('css/chart.css') ?>">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 
 <?php
@@ -76,178 +77,196 @@ foreach ($sj5 as $vj5) {
 				</div>
 				<!-- card body -->
 				<div class="card-body">
-					<div class="title">
-						<center><i class="fas fa-chart-bar fa-fw me-3"></i>Diagram Batang</center>
-					</div>
-					<canvas id="ChartBar" width="400" height="400"></canvas>
-					<script>
-						var ctx = document.getElementById('ChartBar');
-						var ChartBar = new Chart(ctx, {
-							type: 'bar',
-							data: {
-								labels: <?= json_encode($inv); ?>,
-								datasets: [{
-									label: 'R-Barang',
-									data: <?= json_encode($sin); ?>,
-									backgroundColor: [
-										'rgba(255, 99, 132, 0.2)',
-										'rgba(54, 162, 235, 0.2)',
-										'rgba(255, 206, 86, 0.2)',
-										'rgba(75, 192, 192, 0.2)',
-										'rgba(153, 102, 255, 0.2)',
-										'rgba(255, 159, 64, 0.2)'
-									],
-									borderColor: [
-										'rgba(255, 99, 132, 1)',
-										'rgba(54, 162, 235, 1)',
-										'rgba(255, 206, 86, 1)',
-										'rgba(75, 192, 192, 1)',
-										'rgba(153, 102, 255, 1)',
-										'rgba(255, 159, 64, 1)'
-									],
-									borderWidth: 1,
-								}, ],
-							},
-							options: {
-								scales: {
-									y: {
-										beginAtZero: true
-									}
+					<div class="diagram-wrap">
+						<div class="title">
+							<center><i class="fas fa-chart-bar fa-fw me-3"></i>Diagram Batang</center>
+						</div>
+						<canvas class="chartview" id="ChartBar"></canvas>
+						<script>
+							var ctx = document.getElementById('ChartBar');
+							var ChartBar = new Chart(ctx, {
+								type: 'bar',
+								data: {
+									labels: <?= json_encode($inv); ?>,
+									datasets: [{
+										label: 'R-Barang',
+										data: <?= json_encode($sin); ?>,
+										backgroundColor: [
+											'rgba(255, 99, 132, 0.2)',
+											'rgba(54, 162, 235, 0.2)',
+											'rgba(255, 206, 86, 0.2)',
+											'rgba(75, 192, 192, 0.2)',
+											'rgba(153, 102, 255, 0.2)',
+											'rgba(255, 159, 64, 0.2)'
+										],
+										borderColor: [
+											'rgba(255, 99, 132, 1)',
+											'rgba(54, 162, 235, 1)',
+											'rgba(255, 206, 86, 1)',
+											'rgba(75, 192, 192, 1)',
+											'rgba(153, 102, 255, 1)',
+											'rgba(255, 159, 64, 1)'
+										],
+										borderWidth: 1,
+									}, ],
+								},
+								options: {
+									scales: {
+										y: {
+											beginAtZero: true
+										}
+									},
 								}
-							}
-						});
-					</script>
+							});
+						</script>
+					</div>
 					<hr>
 
-					<div class="title">
-						<center><i class="fas fa-chart-line fa-fw me-3"></i>Diagram Garis</center>
-					</div>
-					<canvas id="ChartLine" width="400" height="400"></canvas>
-					<script>
-						var ctx = document.getElementById('ChartLine');
-						var ChartLine = new Chart(ctx, {
-							type: 'line',
-							data: {
-								labels: <?= json_encode($ctg); ?>,
-								datasets: [{
-									label: 'J-Barang',
-									data: <?= json_encode($sct); ?>,
-									backgroundColor: [
-										'rgba(255, 99, 132, 0.2)',
-										'rgba(54, 162, 235, 0.2)',
-										'rgba(255, 206, 86, 0.2)',
-										'rgba(75, 192, 192, 0.2)',
-										'rgba(153, 102, 255, 0.2)',
-										'rgba(255, 159, 64, 0.2)'
-									],
-									borderColor: [
-										'rgba(255, 99, 132, 1)',
-										'rgba(54, 162, 235, 1)',
-										'rgba(255, 206, 86, 1)',
-										'rgba(75, 192, 192, 1)',
-										'rgba(153, 102, 255, 1)',
-										'rgba(255, 159, 64, 1)'
-									],
-									borderWidth: 1,
-								}, ],
-							},
-							options: {
-								scales: {
-									y: {
-										beginAtZero: true
+					<div class="diagram-wrap">
+						<div class="title">
+							<center><i class="fas fa-chart-line fa-fw me-3"></i>Diagram Garis</center>
+						</div>
+						<canvas class="chartview" id="ChartLine"></canvas>
+						<script>
+							var ctx = document.getElementById('ChartLine');
+							var ChartLine = new Chart(ctx, {
+								type: 'line',
+								data: {
+									labels: <?= json_encode($ctg); ?>,
+									datasets: [{
+										label: 'J-Barang',
+										data: <?= json_encode($sct); ?>,
+										backgroundColor: [
+											'rgba(255, 99, 132, 0.2)',
+											'rgba(54, 162, 235, 0.2)',
+											'rgba(255, 206, 86, 0.2)',
+											'rgba(75, 192, 192, 0.2)',
+											'rgba(153, 102, 255, 0.2)',
+											'rgba(255, 159, 64, 0.2)'
+										],
+										borderColor: [
+											'rgba(255, 99, 132, 1)',
+											'rgba(54, 162, 235, 1)',
+											'rgba(255, 206, 86, 1)',
+											'rgba(75, 192, 192, 1)',
+											'rgba(153, 102, 255, 1)',
+											'rgba(255, 159, 64, 1)'
+										],
+										borderWidth: 1,
+									}, ],
+								},
+								options: {
+									scales: {
+										y: {
+											beginAtZero: true
+										}
 									}
 								}
-							}
-						});
-					</script>
+							});
+						</script>
+					</div>
 					<hr>
 
-					<div class="title">
-						<center><i class="fas fa-chart-pie fa-fw me-3"></i>Diagram Pai</center>
-					</div>
-					<canvas id="ChartPie" width="400" height="400"></canvas>
-					<script>
-						var ctx = document.getElementById('ChartPie');
-						var ChartPie = new Chart(ctx, {
-							type: 'pie',
-							data: {
-								labels: <?= json_encode($inv); ?>,
-								datasets: [{
-									label: 'R-Barang',
-									data: <?= json_encode($sin); ?>,
-									backgroundColor: [
-										'rgba(255, 99, 132, 0.2)',
-										'rgba(54, 162, 235, 0.2)',
-										'rgba(255, 206, 86, 0.2)',
-										'rgba(75, 192, 192, 0.2)',
-										'rgba(153, 102, 255, 0.2)',
-										'rgba(255, 159, 64, 0.2)'
-									],
-									borderColor: [
-										'rgba(255, 99, 132, 1)',
-										'rgba(54, 162, 235, 1)',
-										'rgba(255, 206, 86, 1)',
-										'rgba(75, 192, 192, 1)',
-										'rgba(153, 102, 255, 1)',
-										'rgba(255, 159, 64, 1)'
-									],
-									borderWidth: 1,
-								}, ],
-							},
-							options: {
-								scales: {
-									y: {
-										beginAtZero: true
+					<div class="diagram-wrap">
+						<div class="title">
+							<center><i class="fas fa-chart-pie fa-fw me-3"></i>Diagram Pai</center>
+						</div>
+						<canvas class="chartview" id="ChartPie"></canvas>
+						<script>
+							var ctx = document.getElementById('ChartPie');
+							var ChartPie = new Chart(ctx, {
+								type: 'pie',
+								data: {
+									labels: <?= json_encode($inv); ?>,
+									datasets: [{
+										label: 'R-Barang',
+										data: <?= json_encode($sin); ?>,
+										backgroundColor: [
+											'rgba(255, 99, 132, 0.2)',
+											'rgba(54, 162, 235, 0.2)',
+											'rgba(255, 206, 86, 0.2)',
+											'rgba(75, 192, 192, 0.2)',
+											'rgba(153, 102, 255, 0.2)',
+											'rgba(255, 159, 64, 0.2)'
+										],
+										borderColor: [
+											'rgba(255, 99, 132, 1)',
+											'rgba(54, 162, 235, 1)',
+											'rgba(255, 206, 86, 1)',
+											'rgba(75, 192, 192, 1)',
+											'rgba(153, 102, 255, 1)',
+											'rgba(255, 159, 64, 1)'
+										],
+										borderWidth: 1,
+									}, ],
+								},
+								options: {
+									scales: {
+										y: {
+											beginAtZero: true,
+										}
+									},
+									legend: {
+										display: true,
+										position: "right",
 									}
 								}
-							}
-						});
-					</script>
+							});
+						</script>
+					</div>
 					<hr>
 
-					<div class="title">
-						<center><i class="fas fa-dot-circle fa-fw me-3"></i>Diagram Donat</center>
-					</div>
-					<canvas id="ChartDonuts" width="400" height="400"></canvas>
-					<script>
-						var ctx = document.getElementById('ChartDonuts');
-						var ChartDonuts = new Chart(ctx, {
-							type: 'doughnut',
-							data: {
-								labels: <?= json_encode($ctg); ?>,
-								datasets: [{
-									label: 'J-Barang',
-									data: <?= json_encode($sct); ?>,
-									backgroundColor: [
-										'rgba(255, 99, 132, 0.2)',
-										'rgba(54, 162, 235, 0.2)',
-										'rgba(255, 206, 86, 0.2)',
-										'rgba(75, 192, 192, 0.2)',
-										'rgba(153, 102, 255, 0.2)',
-										'rgba(255, 159, 64, 0.2)'
-									],
-									borderColor: [
-										'rgba(255, 99, 132, 1)',
-										'rgba(54, 162, 235, 1)',
-										'rgba(255, 206, 86, 1)',
-										'rgba(75, 192, 192, 1)',
-										'rgba(153, 102, 255, 1)',
-										'rgba(255, 159, 64, 1)'
-									],
-									borderWidth: 1,
-								}, ],
-							},
-							options: {
-								scales: {
-									y: {
-										beginAtZero: true
+					<div class="diagram-wrap">
+						<div class="title">
+							<center><i class="fas fa-dot-circle fa-fw me-3"></i>Diagram Donat</center>
+						</div>
+						<canvas class="chartview" id="ChartDonuts"></canvas>
+						<script>
+							var ctx = document.getElementById('ChartDonuts');
+							var ChartDonuts = new Chart(ctx, {
+								type: 'doughnut',
+								data: {
+									labels: <?= json_encode($ctg); ?>,
+									datasets: [{
+										label: 'J-Barang',
+										data: <?= json_encode($sct); ?>,
+										backgroundColor: [
+											'rgba(255, 99, 132, 0.2)',
+											'rgba(54, 162, 235, 0.2)',
+											'rgba(255, 206, 86, 0.2)',
+											'rgba(75, 192, 192, 0.2)',
+											'rgba(153, 102, 255, 0.2)',
+											'rgba(255, 159, 64, 0.2)'
+										],
+										borderColor: [
+											'rgba(255, 99, 132, 1)',
+											'rgba(54, 162, 235, 1)',
+											'rgba(255, 206, 86, 1)',
+											'rgba(75, 192, 192, 1)',
+											'rgba(153, 102, 255, 1)',
+											'rgba(255, 159, 64, 1)'
+										],
+										borderWidth: 1,
+									}, ],
+								},
+								options: {
+									scales: {
+										y: {
+											beginAtZero: true
+										}
+									},
+									legend: {
+										display: true,
+										position: "bottom",
 									}
 								}
-							}
-						});
-					</script>
+							});
+						</script>
+					</div>
+					<hr>
 				</div>
-				<!-- end card body -->
+			</div>
+			<!-- end card body -->
 		</section>
 	</div>
 </main>
