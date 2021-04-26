e60c2c8a9b98f8a11968982d3228ed42ba5e8c10
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-04-27 00:10:45 +0700

    Update : Tampilan Footer Home

--- app/Views/home/landingFooter.php
+++ app/Views/home/landingFooter.php
@@ -3,53 +3,60 @@
         <!-- Footer Links -->
         <div class="container text-center text-md-left">
             <!-- Grid row -->
-            <div class="row">
+            <div class="row row-footer">
                 <!-- Grid column -->
                 <div class="col-md-6 mt-md-0 mt-3">
                     <!-- Content -->
-                    <h5 class="text-uppercase">Footer Content</h5>
-                    <p>Here you can use rows and columns here to organize your footer content.</p>
+                    <h5 class="text-uppercase">CONTACT</h5>
+
+                    <div class="card-body sosmed-home">
+                        <?php
+                        $mail = "https://mail.google.com/mail/";
+                        $emailtujuan = "invenbar@invweb.ac.id";
+                        $subject = "HUBUNGI ADMIN INVENBAR";
+                        $enter = "%0A%0A%0A";
+                        $pg1 = "[Tuliskan pesan anda disini]";
+                        $pg2 = "Dengan ini, saya menyatakan bahwa saya menghubungi admin tanpa paksaan siapapun serta dalam keadaan sadar.";
+                        $pg3 = "Atas perhatian Bapak/Ibu, besar harapan saya agar permintaan saya dapat diproses sebagaimana mestinya. Sekian dari saya, terima kasih.";
+                        $namaAnda = "[Nama Anda]";
+                        $body = "Isi Pesan:%0A" . $pg1 . "" . $enter . "" . $pg2 . "%0A" . $pg3 . "%0A%0AHormat Saya," . $enter . "" . $namaAnda;
+
+                        $pesan = "" . $mail . "?view=cm&fs=1&tf=1&to=" . $emailtujuan . "&subject?&su=" . $subject . "&body=" . $body;
+                        ?>
+
+                        <a href="<?= $pesan; ?>"><i class="fas fa-envelope fa-fw me-1"></i>Email</a>
+                        <a href="#" onclick="return false;"><i class="fab fa-facebook-square fa-fw me-1"></i>Facebook</a>
+                        <a href="#" onclick="return false;"><i class="fab fa-twitter-square fa-fw me-1"></i>Twitter</a>
+                        <a href="#" onclick="return false;"><i class="fab fa-instagram-square fa-fw me-1"></i>Instagram</a>
+                    </div>
                 </div>
+
                 <!-- Grid column -->
-                <hr class="clearfix w-100 d-md-none pb-3">
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
+                <div class="col-md-3 mb-md-0 mb-3 network">
                     <!-- Links -->
-                    <h5 class="text-uppercase">Links</h5>
+                    <h5 class="text-uppercase">NETWORK</h5>
 
                     <ul class="list-unstyled">
                         <li>
-                            <a href="#!">Link 1</a>
+                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Invenbar Info</a>
                         </li>
                         <li>
-                            <a href="#!">Link 2</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 3</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
+                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Partnership</a>
                         </li>
                     </ul>
                 </div>
+
                 <!-- Grid column -->
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
+                <div class="col-md-3 mb-md-0 mb-3 our-website">
                     <!-- Links -->
-                    <h5 class="text-uppercase">Links</h5>
+                    <h5 class="text-uppercase">OUR WEBSITE</h5>
 
                     <ul class="list-unstyled">
                         <li>
-                            <a href="#!">Link 1</a>
+                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Kebijakan Privasi</a>
                         </li>
                         <li>
-                            <a href="#!">Link 2</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 3</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
+                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Waspada Penipuan</a>
                         </li>
                     </ul>
                 </div>
