2f3156e0f41778324b52468175cad50bcf153ba2
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-04-27 00:11:27 +0700

    Update : Tampilan Navbar Home

--- app/Views/home/landingNavbar.php
+++ app/Views/home/landingNavbar.php
@@ -21,31 +21,6 @@
                        <li class="nav-item active">
                            <a href="<?= session('uid') == null ? base_url("login") : base_url("/Menu/dashboard") ?>" class="nav-link"><?= session('uid') == null ? 'Masuk' : 'Dashboard' ?></a>
                        </li>
-                       <li class="nav-item dropdown">
-                           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                               Tentang
-                           </a>
-                           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                               <a class="dropdown-item" type="button" data-toggle="modal" data-target="#TentangModal">Anggota</a>
-                               <a class="dropdown-item">Tugas</a>
-                               <?php
-                                $mail = "https://mail.google.com/mail/";
-                                $emailtujuan = "fikinvenbar@upnjatim.ac.id";
-                                $subject = "HUBUNGI ADMIN INVENBAR";
-                                $enter = "%0A%0A%0A";
-                                $pg1 = "[Tuliskan pesan anda disini]";
-                                $pg2 = "Dengan ini, saya menyatakan bahwa saya menghubungi admin tanpa paksaan siapapun serta dalam keadaan sadar.";
-                                $pg3 = "Atas perhatian Bapak/Ibu, besar harapan saya agar permintaan saya dapat diproses sebagaimana mestinya. Sekian dari saya, terima kasih.";
-                                $namaAnda = "[Nama Anda]";
-                                $body = "Isi Pesan:%0A" . $pg1 . "" . $enter . "" . $pg2 . "%0A" . $pg3 . "%0A%0AHormat Saya," . $enter . "" . $namaAnda;
-
-                                $pesan = "" . $mail . "?view=cm&fs=1&tf=1&to=" . $emailtujuan . "&subject?&su=" . $subject . "&body=" . $body;
-                                ?>
-                               <a class="dropdown-item reg" href="<?= $pesan; ?>" target="_blank" http-equiv="refresh" content="2">Kontak</a>
-                           </div>
-                       </li>
-                       <li class="nav-item">
-                       </li>
                    </ul>
                </div>
            </div>
