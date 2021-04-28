9db4a283613f1b0c9fad9af3e71cb947c7451ef8
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-04-28 17:32:05 +0700

    Update : Konten dan desain main home

--- app/Views/home/landingMain.php
+++ app/Views/home/landingMain.php
@@ -21,7 +21,7 @@
                     barang ini dapat mempertahankan performa bisnis anda dengan tetap pegang kendali penuh pada persediaan stok barang yang tersaji
                     cepat dan real time. Dengan kemudahan yang didapatkan, jadikanlah Invenbar sebagai partner bisnis anda mulai dari sekarang...
                 </p>
-                <a class="btn btn-info shadow-sm">Detail</a>
+                <a href="<?= base_url("login"); ?>" class="btn mulai btn-info shadow-sm" style="letter-spacing:3px;font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Mulai&nbsp;Inventaris</a>
             </div>
         </div><br><br>
 
@@ -38,71 +38,64 @@
                             <div class="carousel-inner">
                                 <div class="carousel-item active" data-interval="5000">
                                     <div class="card-deck">
-                                        <div class="con-article col-md-4 mb-6">
-                                            <div class="card">
-                                                <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/login.jpg"); ?>">
-                                                <div class="card-body">
-                                                    <h4 class="card-title">LOGIN</h4>
-                                                    <p class="card-text">Login Invenbar merupakan sebuah menu yang tersedia sebagai sarana untuk masuk ke akun pengguna website inventaris barang gudang.</p>
-                                                    <a href="<?= base_url("login"); ?>">Kunjungi</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="con-article col-md-4">
+                                        <div class="con-article col-md-4 mb-2">
                                             <div class="card">
                                                 <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/dashboard.jpg"); ?>">
                                                 <div class="card-body">
                                                     <h4 class="card-title">DASHBOARD</h4>
                                                     <p class="card-text">Dashboard Invenbar merupakan sebuah menu yang memiliki manfaat yaitu dapat memberikan informasi dengan mudah dan ringkas pada satu halaman.</p>
-                                                    <a href="<?= base_url("/Menu/dashboard"); ?>">Kunjungi</a>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="con-article col-md-4">
+                                        </div><br><br>
+                                        <div class="con-article col-md-4 mb-2">
+                                            <div class="card">
+                                                <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/login.jpg"); ?>">
+                                                <div class="card-body">
+                                                    <h4 class="card-title">KELOLA BARANG</h4>
+                                                    <p class="card-text">Kelola Barang Invenbar merupakan sebuah menu yang tersedia sebagai sarana untuk mengelola barang oleh pengguna website.</p>
+                                                </div>
+                                            </div>
+                                        </div><br><br>
+                                        <div class="con-article col-md-4 mb-2">
                                             <div class="card">
                                                 <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/viewchart.jpg"); ?>">
                                                 <div class="card-body">
-                                                    <h4 class="card-title">VIEW CHART</h4>
-                                                    <p class="card-text">View Chart Invenbar merupakan sebuah menu yang berfungsi menampilkan data stok anda dalam bentuk beberapa diagram.</p>
-                                                    <a href="<?= base_url("/Menu/chart"); ?>">Kunjungi</a>
+                                                    <h4 class="card-title">ABSENSI PEKERJA</h4>
+                                                    <p class="card-text">Absensi Pekerja Invenbar merupakan sebuah menu yang berfungsi untuk mendata kehadiran setiap pengguna website.</p>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div><br><br>
                                     </div>
                                 </div>
                                 <div class="carousel-item" data-interval="5000">
                                     <div class="card-group">
-                                        <div class="con-article col-md-4">
+                                        <div class="con-article col-md-4 mb-2">
                                             <div class="card">
                                                 <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/pengumuman.jpg"); ?>">
                                                 <div class="card-body">
                                                     <h4 class="card-title">PENGUMUMAN</h4>
                                                     <p class="card-text">Pengumuman Invenbar merupakan sebuah menu yang dibuat bertujuan agar pengguna dapat mengetahui informasi dari Admin.</p>
-                                                    <a href="<?= base_url("/Menu/pengumuman") ?>">Kunjungi</a>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="con-article col-md-4">
-                                            <div class="card">
-                                                <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/absensi.jpg"); ?>">
-                                                <div class="card-body">
-                                                    <h4 class="card-title">ABSENSI PEKERJA</h4>
-                                                    <p class="card-text">Absensi Pekerja Invenbar merupakan sebuah menu yang ditujukan untuk pengguna agar dapat melakukan absensi.</p>
-                                                    <a href="<?= base_url("/Menu/absensiUser") ?>">Kunjungi</a>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                        <div class="con-article col-md-4">
+                                        </div><br><br>
+                                        <div class="con-article col-md-4 mb-2">
                                             <div class="card">
                                                 <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/laporanbulanan.jpg"); ?>">
                                                 <div class="card-body">
-                                                    <h4 class="card-title">CETAK LAPORAN BULANAN</h4>
-                                                    <p class="card-text">Cetak Laporan Bulanan merupakan sebuah menu yang ditujukan untuk pengguna agar dapat melakukan cetak laporan bulanan.</p>
-                                                    <a href="<?= base_url("/Menu/laporanBulanan") ?>">Kunjungi</a>
+                                                    <h4 class="card-title">CETAK LAPORAN</h4>
+                                                    <p class="card-text">Cetak Laporan Invenbar merupakan sebuah menu yang ditujukan untuk pengguna agar dapat melakukan cetak laporan bulanan.</p>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div><br><br>
+                                        <div class="con-article col-md-4 mb-2">
+                                            <div class="card">
+                                                <img class="img-fluid" alt="100%x280" src="<?= base_url("img/home/absensi.jpg"); ?>">
+                                                <div class="card-body">
+                                                    <h4 class="card-title">PENGADUAN</h4>
+                                                    <p class="card-text">Pengaduan Invenbar merupakan sebuah menu yang ditujukan untuk pengguna agar dapat menginformasikan langsung kepada admin terkait kendala yang ada.</p>
+                                                </div>
+                                            </div>
+                                        </div><br><br>
                                     </div>
                                 </div>
                             </div>
