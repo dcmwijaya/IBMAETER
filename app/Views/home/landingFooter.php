a5a43c7b3cd6515c177236e3f2a7fbf2ce653c03
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-04-28 17:09:44 +0700

    Update : Footer Home

--- app/Views/home/landingFooter.php
+++ app/Views/home/landingFooter.php
@@ -1,68 +1,71 @@
-    <!-- Footer -->
-    <footer class="page-footer font-small bg-dark text-light pt-4 mt-4">
-        <!-- Footer Links -->
-        <div class="container text-center text-md-left">
-            <!-- Grid row -->
-            <div class="row row-footer">
-                <!-- Grid column -->
-                <div class="col-md-6 mt-md-0 mt-3">
-                    <!-- Content -->
-                    <h5 class="text-uppercase">CONTACT</h5>
+<?php
+$mail = "https://mail.google.com/mail/";
+$emailtujuan = "invenbar@invweb.ac.id";
+$subject = "HUBUNGI ADMIN INVENBAR";
+$enter = "%0A%0A%0A";
+$pg1 = "[Tuliskan pesan anda disini]";
+$pg2 = "Dengan ini, saya menyatakan bahwa saya menghubungi admin tanpa paksaan siapapun serta dalam keadaan sadar.";
+$pg3 = "Atas perhatian Bapak/Ibu, besar harapan saya agar permintaan saya dapat diproses sebagaimana mestinya. Sekian dari saya, terima kasih.";
+$namaAnda = "[Nama Anda]";
+$body = "Isi Pesan:%0A" . $pg1 . "" . $enter . "" . $pg2 . "%0A" . $pg3 . "%0A%0AHormat Saya," . $enter . "" . $namaAnda;
 
-                    <div class="card-body sosmed-home">
-                        <?php
-                        $mail = "https://mail.google.com/mail/";
-                        $emailtujuan = "invenbar@invweb.ac.id";
-                        $subject = "HUBUNGI ADMIN INVENBAR";
-                        $enter = "%0A%0A%0A";
-                        $pg1 = "[Tuliskan pesan anda disini]";
-                        $pg2 = "Dengan ini, saya menyatakan bahwa saya menghubungi admin tanpa paksaan siapapun serta dalam keadaan sadar.";
-                        $pg3 = "Atas perhatian Bapak/Ibu, besar harapan saya agar permintaan saya dapat diproses sebagaimana mestinya. Sekian dari saya, terima kasih.";
-                        $namaAnda = "[Nama Anda]";
-                        $body = "Isi Pesan:%0A" . $pg1 . "" . $enter . "" . $pg2 . "%0A" . $pg3 . "%0A%0AHormat Saya," . $enter . "" . $namaAnda;
+$pesan = "" . $mail . "?view=cm&fs=1&tf=1&to=" . $emailtujuan . "&subject?&su=" . $subject . "&body=" . $body;
+?>
 
-                        $pesan = "" . $mail . "?view=cm&fs=1&tf=1&to=" . $emailtujuan . "&subject?&su=" . $subject . "&body=" . $body;
-                        ?>
+<footer id="footer">
 
-                        <a href="<?= $pesan; ?>"><i class="fas fa-envelope fa-fw me-1"></i>Email</a>
-                        <a href="#" onclick="return false;"><i class="fab fa-facebook-square fa-fw me-1"></i>Facebook</a>
-                        <a href="#" onclick="return false;"><i class="fab fa-twitter-square fa-fw me-1"></i>Twitter</a>
-                        <a href="#" onclick="return false;"><i class="fab fa-instagram-square fa-fw me-1"></i>Instagram</a>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="f1-content col-lg-3 col-md-6 footer-contact">
+                    <h3>INVENBAR</h3>
+                    <p>
+                        Website Inventaris Barang Gudang<br><br>
+                        <strong>Phone:</strong> (+62)838-3747-1292<br>
+                        <strong>Email:</strong> invenbar@invweb.ac.id<br>
+                    </p>
+                </div>
+                <hr class="line">
+                <div class="f2-content f-kolom col-lg-3 col-md-6 footer-links">
+                    <h4>NETWORK</h4>
+                    <ul>
+                        <li><a href="<?= base_url("menu/InfoHome"); ?>"><i class="fas fa-caret-right fa-fw me-1" style="color: #001158;"></i>Invenbar Info</a></li>
+                        <li><a href="<?= base_url("menu/Partnership"); ?>"><i class="fas fa-caret-right fa-fw me-1" style="color: #001158;"></i>Partnership</a></li>
+                    </ul>
+                </div>
+
+                <div class="f3-content f-kolom col-lg-3 col-md-6 footer-links">
+                    <h4>OUR WEBSITE</h4>
+                    <ul>
+                        <li><a href="<?= base_url("menu/KebijakanPrivasi"); ?>"><i class="fas fa-caret-right fa-fw me-1" style="color: #001158;"></i>Kebijakan Privasi</a></li>
+                        <li><a href="<?= base_url("menu/WaspadaPenipuan"); ?>"><i class="fas fa-caret-right fa-fw me-1" style="color: #001158;"></i>Waspada Penipuan</a></li>
+                    </ul>
+                </div>
+                <hr class="line">
+                <div class="f4-content col-lg-3 col-md-6 footer-links">
+                    <h4>CONTACT</h4>
+                    <p>Hubungi sosial media kami dibawah ini</p>
+                    <div class="social-links mt-3">
+                        <a href="<?= $pesan; ?>" class="google-mail"><i class="fas fa-envelope"></i></a>
+                        <a href="#" class="twitter" onclick="return false;"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="facebook" onclick="return false;"><i class="fab fa-facebook"></i></a>
+                        <a href="#" class="instagram" onclick="return false;"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="linkedin" onclick="return false;"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
 
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3 network">
-                    <!-- Links -->
-                    <h5 class="text-uppercase">NETWORK</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Invenbar Info</a>
-                        </li>
-                        <li>
-                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Partnership</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3 our-website">
-                    <!-- Links -->
-                    <h5 class="text-uppercase">OUR WEBSITE</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Kebijakan Privasi</a>
-                        </li>
-                        <li>
-                            <a href="#" onclick="return false;"><i class="fas fa-caret-right fa-fw me-1"></i>Waspada Penipuan</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </div>
-        <div class="footer-copyright text-center py-3 small">@ 2021 :
-            <a href="<?= base_url('/'); ?>"> Kelompok 5 Framework C</a>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+        <div class="copyright">
+            &copy; 2021 <strong><span>Invenbar</span></strong>. All Rights Reserved
         </div>
-    </footer>
+        <div class="credits">
+            Designed by <strong>kelompok-5</strong>
+        </div>
+    </div>
+
+</footer>
