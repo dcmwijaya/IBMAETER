5da4a966ac4203227fc95780e90595fc10968f3e
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-05-01 17:46:37 +0700

    Update laporanBulanan.php

--- app/Views/global/laporanBulanan.php
+++ app/Views/global/laporanBulanan.php
@@ -1,7 +1,8 @@
 <?= $this->extend('layout/template') ?>
 
 <?= $this->section('content') ?>
-<link rel="stylesheet" href="<?= base_url('../css/content.css') ?>" /> <!-- include cakra --->
+<link rel="stylesheet" href="<?= base_url('css/menukhusus.css') ?>">
+
 <!--Main layout-->
 <main class="bg-dark">
 	<div class="container pt-4">
@@ -9,43 +10,44 @@
 			<div class="card">
 				<div class="card-header text-center py-3">
 					<h5 class="mb-0 text-center">
-						<strong>CETAK LAPORAN BULANAN</strong>
+						<strong>LAPORAN BULANAN</strong>
 					</h5>
 				</div>
 				<div class="card-body pt-1">
 					<div class="container mb-3 pb-2" style="border-bottom: 1px solid #dfdfdf;">
 						<div class="row">
-							<div class="col">
-								<marquee>
-									<img src="<?= base_url('img/development.gif') ?>">
-									<h1 class="blink">Dalam proses pengembangan...</h1>
-									</img>
-								</marquee>
-								<style>
-									.blink {
-										animation: blink-animation 1s steps(5, start) infinite;
-										-webkit-animation: blink-animation 1s steps(5, start) infinite;
-										color: yellow;
-									}
-
-									@keyframes blink-animation {
-										to {
-											visibility: hidden;
-											color: blue;
-										}
-									}
-
-									@-webkit-keyframes blink-animation {
-										to {
-											visibility: hidden;
-											color: yellow;
-										}
-									}
-								</style>
+							<div class="info-card card mb-3 bg-light">
+								<div class="row no-gutters">
+									<div class="col-md-5" id="img-laporanbulanan">
+										<img src="<?= base_url("img/LaporanBulanan.jpg"); ?>" class="card-img" alt="img-1">
+									</div>
+									<div class="col-md-6" id="laporanbulanan-user">
+										<div class=" card-body event-description">
+											<form action=" <?= base_url('/Menu/LaporanBulanan'); ?>" method="POST" enctype="multipart/form-data">
+												<div class="form-group laporanbulanan-content">
+													<label for="ItemTime_before" class="font-weight-bold">Date Before</label>
+													<input type="datetime-local" class="form-control" value="" id="Time_before" name="tgl_before" required>
+												</div>
+												<div class="form-group laporanbulanan-content">
+													<label for="ItemTime_after" class="font-weight-bold">Date After</label>
+													<input type="datetime-local" class="form-control" value="" id="Time_after" name="tgl_after" required>
+												</div>
+												<div class="my-4 laporan-button">
+													<input type="hidden" class="form-control" value="" name="id_absen">
+													<button type="submit" class="btn btn-success btn-komplain col-sm-12 p-4">
+														<p class="text-center ntombol"><i class="fas fa-print fa-fw me-3"></i>Cetak Laporan Bulanan</p>
+													</button>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
+
 					</div>
 				</div>
+			</div>
 		</section>
 	</div><br>
 </main>
