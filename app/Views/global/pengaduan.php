886aa0064f683046c75a93cc0629ac380dd2d373
DEVAN CAKRA MUDRA WIJAYA <54527592+devancakra@users.noreply.github.com>
2021-05-01 13:50:26 +0700

    Update pengaduan.php

--- app/Views/global/pengaduan.php
+++ app/Views/global/pengaduan.php
@@ -1,7 +1,9 @@
 <?= $this->extend('layout/template') ?>
 
 <?= $this->section('content') ?>
-<link rel="stylesheet" href="<?= base_url('../css/content.css') ?>" /> <!-- include cakra --->
+<link rel="stylesheet" href="<?= base_url('css/menukhusus.css') ?>">
+<link rel="stylesheet" href="<?= base_url('css/dragdrop.css') ?>" />
+
 <!--Main layout-->
 <main class="bg-dark">
 	<div class="container pt-4">
@@ -15,37 +17,46 @@
 				<div class="card-body pt-1">
 					<div class="container mb-3 pb-2" style="border-bottom: 1px solid #dfdfdf;">
 						<div class="row">
-							<div class="col">
-								<marquee>
-									<img src="<?= base_url('img/development.gif') ?>">
-									<h1 class="blink">Dalam proses pengembangan...</h1>
-									</img>
-								</marquee>
-								<style>
-									.blink {
-										animation: blink-animation 1s steps(5, start) infinite;
-										-webkit-animation: blink-animation 1s steps(5, start) infinite;
-										color: yellow;
-									}
-
-									@keyframes blink-animation {
-										to {
-											visibility: hidden;
-											color: blue;
-										}
-									}
-
-									@-webkit-keyframes blink-animation {
-										to {
-											visibility: hidden;
-											color: yellow;
-										}
-									}
-								</style>
+							<div class="info-card card mb-3 bg-light">
+								<div class="row no-gutters">
+									<div class="col-md-5" id="img-pengaduan">
+										<img src="<?= base_url("img/home/dashboard.jpg"); ?>" class="card-img" alt="img-1">
+									</div>
+									<div class="col-md-6" id="pengaduan-user">
+										<div class=" card-body event-description">
+											<form action=" <?= base_url('/Menu/Pengaduan'); ?>" method="POST" enctype="multipart/form-data">
+												<input type="hidden" name="fotoLama" value="">
+												<div class="form-group">
+													<label for="judul_komplain"><b>Judul Komplain</b></label>
+													<input type="text" class="form-control col-sm-12 p-2" id="judul_komplain" name="judulKomplain" placeholder="Tuliskan judul komplain..." value="">
+												</div>
+												<div class="form-group">
+													<label for="isi_komplain"><b>Isi Komplain</b></label>
+													<small class="text-muted"><span style="color: red;">*</span> Maksimal 256 huruf</small>
+													<textarea class="col-sm-12 p-2" id="isi_komplain" rows="6" name="isi" placeholder="Sampaikan isi komplain..."></textarea>
+												</div>
+												<div class="form-group">
+													<label for="edit_gambar_info"><b>Bukti Screenshoot</b></label>
+													<small class="text-muted"><span style="color: red;">*</span> Gambar (.jpg/.jpeg/.png) maks 10Mb</small>
+													<div class="drop-zone col-sm-12">
+														<span class="drop-zone__prompt">Drop file here or click to upload</span>
+														<input type="file" name="foto" class="drop-zone__input" />
+													</div>
+												</div>
+												<script src="<?= base_url('../js/dragdrop.js'); ?>"></script>
+												<div class="my-4">
+													<input type="hidden" class="form-control" value="" name="id_pengaduan">
+													<button type="submit" class="btn btn-success btn-komplain col-sm-12"><i class="fas fa-fw fa-check"></i> Kirim Komplain</button>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
 		</section>
 	</div><br>
 </main>
