fd3dde3ff47e17f45869c42c3429ee43a938d1e7
merdinabrori <merdin.abrori@gmail.com>
2021-05-19 10:04:28 +0700

    Pengubahan timezone

--- app/Config/App.php
+++ app/Config/App.php
@@ -108,7 +108,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $appTimezone = 'America/Chicago';
+	public $appTimezone = 'Asia/Jakarta';
 
 	/**
 	 * --------------------------------------------------------------------------
@@ -303,7 +303,7 @@ class App extends BaseConfig
 	 * (empty string) means no SameSite attribute will be set on cookies. If
 	 * set to `None`, `$cookieSecure` must also be set.
 	 *
-       * @var string 'Lax'|'None'|'Strict'
+	 * @var string 'Lax'|'None'|'Strict'
 	 */
 	public $cookieSameSite = 'Lax';
 
