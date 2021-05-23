3bc1a18c9a8c44025f6cb5f5beadce9c1d516bf2
RifkyA911 <rifkyakhmad911@gmail.com>
2021-05-23 14:21:27 +0700

    Error Phpmyadmon :(

--- app/Views/admin/pengumuman_part/delete_form.php
+++ app/Views/admin/pengumuman_part/delete_form.php
@@ -4,17 +4,17 @@
         <div class="col-sm-8">
             <div class="form-group">
                 <label for="pengumuman_judul"><i class="fas fa-fw fa-thumbtack"></i><b> Judul Pengumuman</b></label>
-                <input type="text" class="form-control" id="pengumuman_judul" placeholder="Edit Judul Pengumuman..." name="judul" required>
+                <input type="text" class="form-control" id="pengumuman_judul" name="judul" readonly>
             </div>
             <div class="form-group">
                 <label for="pengumuman_isi"><i class="fas fa-fw fa-comment-dots"></i><b> Isi Pengumuman</b></label>
-                <textarea class="form-control" id="pengumuman_isi" rows="6" name="isi" placeholder="Sampaikan isi pengumuman...&#10;" required></textarea>
+                <textarea class="form-control" id="pengumuman_isi" rows="6" name="isi" readonly></textarea>
                 <small class="text-muted"><span style="color: red;">*</span> Maksimal 254 huruf</small>
             </div>
         </div>
         <div class="col-sm-4 border-left p-4">
             <div class="form-group">
-                <button type="button" id="info-clear" class="btn btn-danger btn-block p-2 shadow-sm" onclick="infoClear()"><i class="fas fa-fw fa-trash"></i> Kosongkan</button>
+                <button type="button" id="info-clear" class="btn btn-danger btn-block p-2 shadow-sm disabled" disabled><i class="fas fa-fw fa-trash"></i> Kosongkan</button>
             </div>
         </div>
     </div>
