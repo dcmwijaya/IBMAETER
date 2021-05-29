48f708e22d0b498060d899ac62dd680dcd0178be
devancakra <mudrawijaya@gmail.com>
2021-05-29 15:38:30 +0700

    update: ui website

--- app/Views/home/landingNavbar.php
+++ app/Views/home/landingNavbar.php
@@ -14,12 +14,18 @@
                </button>
                <div class="collapse navbar-collapse" id="navbarText">
                    <span class="navbar-text mr-auto"></span>
-                   <ul class="navbar-nav ">
-                       <li class="nav-item active">
-                           <a href="<?= base_url("home") ?>" class="nav-link">Home</a>
+                   <ul class="navbar-nav">
+                       <li class="nav-item active me-3">
+                           <a href="<?= base_url("home") ?>" class="nav-link"><i class="fas fa-home fa-fw me-1"></i>Home</a>
                        </li>
-                       <li class="nav-item active">
-                           <a href="<?= session('uid') == null ? base_url("login") : base_url("/Menu/dashboard") ?>" class="nav-link"><?= session('uid') == null ? 'Masuk' : 'Dashboard' ?></a>
+                       <li class="nav-item active me-3">
+                           <a href="<?= base_url("home/KebijakanPrivasi") ?>" class="nav-link"><i class="fas fa-user-shield fa-fw me-1"></i>Kebijakan Privasi</a>
+                       </li>
+                       <li class="nav-item active me-3">
+                           <a href="<?= base_url("home/WaspadaPenipuan") ?>" class="nav-link"><i class="fas fa-skull fa-fw me-1"></i>Waspada Penipuan</a>
+                       </li>
+                       <li class="nav-item active me-3">
+                           <a href="<?= session('uid') == null ? base_url("login") : base_url("/Menu/dashboard") ?>" class="nav-link"><i class="fas fa-sign-in-alt fa-fw me-1"></i><?= session('uid') == null ? 'Masuk' : 'Dashboard' ?></a>
                        </li>
                    </ul>
                </div>
