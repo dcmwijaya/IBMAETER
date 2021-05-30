33b784d496ddc4532064e472fe95475478ab3ddd
devancakra <mudrawijaya@gmail.com>
2021-05-30 09:38:37 +0700

    update: penataan SQL

--- public/sql/trigger del_komp_visib.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER $$
-
-CREATE OR REPLACE TRIGGER `del_komp_visib`
-AFTER DELETE ON `komplain`
-FOR EACH ROW
-BEGIN
-DELETE FROM `komplain_visibility` WHERE `no_komplain` = old.`no_komplain`;
-END$$
-DELIMITER ;
--- public/sql/trigger del_pengumuman_visib.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER $$
-
-CREATE OR REPLACE TRIGGER `del_pengumuman_visib`
-AFTER DELETE ON `pengumuman`
-FOR EACH ROW
-BEGIN
-DELETE FROM `pengumuman_visibility` WHERE `id_pengumuman` = old.`id_pengumuman`;
-END$$
-DELIMITER ;
