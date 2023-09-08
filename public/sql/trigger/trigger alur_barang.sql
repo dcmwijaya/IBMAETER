0afdd96ad1657b26ce185503f18241bdd5ee4b86
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2023-09-08 10:11:52 +0700

    Update trigger alur_barang.sql
    mengubah new.nama_barang menjadi new.nama_item karena kolom nama_barang tidak ada di Database

--- public/sql/trigger/trigger alur_barang.sql
+++ public/sql/trigger/trigger alur_barang.sql
@@ -6,10 +6,10 @@ CREATE TRIGGER `stok_dinamis` AFTER UPDATE ON `alur_barang` FOR EACH ROW BEGIN
 IF(new.`status`="Diterima") THEN
 IF (new.`request`="Masuk") THEN
 	UPDATE `item` SET `item`.`stok` = `item`.`stok` + new.`ubah_stok`
-	WHERE `item`.`nama_item` = new.`nama_barang`;
+	WHERE `item`.`nama_item` = new.`item`.`nama_item`;
 ELSEIF(new.`request`="Keluar") THEN
 UPDATE `item` SET `item`.`stok` = `item`.`stok` - new.`ubah_stok`
-	WHERE `item`.`nama_item` = new.`nama_barang`;
+	WHERE `item`.`nama_item` = new.`item`.`nama_item`;
     END IF;
     END IF;
     END
